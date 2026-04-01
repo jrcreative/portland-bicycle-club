87c79e13d0fb2d37b8aa136e71cf43864f60425c
jrcreative <jeremy@indevver.com>
2026-04-01 14:52:31 -0700

    update files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,53 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f3cdb8236d193186c17a79520c76ef9abca39a21',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f3cdb8236d193186c17a79520c76ef9abca39a21',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpackagist-plugin/advanced-custom-fields' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '6.7.1',
+            ),
+        ),
+        'wpengine/advanced-custom-fields' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '6.7.1',
+            ),
+        ),
+        'wpengine/advanced-custom-fields-pro' => array(
+            'pretty_version' => '6.7.1',
+            'version' => '6.7.1.0',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../wp-content/plugins/advanced-custom-fields-pro',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
