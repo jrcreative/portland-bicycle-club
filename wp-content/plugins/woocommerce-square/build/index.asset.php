d4bffb671890a8ea58f8732fb9527cf0906de84f
jrcreative <jeremy@indevver.com>
2026-04-20 15:58:04 -0700

    add wc square

---
+++ wp-content/plugins/woocommerce-square/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-i18n'), 'version' => '6d45318c630515d96e0a');
---
+++ wp-content/plugins/woocommerce-square/build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-tracks', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3dd3005fa9a198aab0fc');
