004d32425025de53ae0d9565afe86df437ff0759
Mark Hartel <mark_hartel@hotmail.com>
2026-08-14 21:10:08 -0700

    Users must be logged in to view authors

--- wp-content/themes/pbc/resources/author.php
+++ wp-content/themes/pbc/resources/author.php
@@ -4,12 +4,14 @@ use Timber\Timber;
 
 $data = Timber::context();
 
-$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
-
-$data['id'] = $curauth->ID;
-$data['nickname'] = $curauth->nickname;
-$data['first_name'] = $curauth->first_name;
-$data['last_name'] = $curauth->last_name;
-
-// render
-Timber::render('pages/author.html.twig', $data);
+if (is_user_logged_in()) {
+    $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+    $data['id'] = $curauth->ID;
+    $data['nickname'] = $curauth->nickname;
+    $data['first_name'] = $curauth->first_name;
+    $data['last_name'] = $curauth->last_name;
+    Timber::render('pages/author.html.twig', $data);
+}
+else {
+    Timber::render('pages/404.html.twig', $data);
+}
